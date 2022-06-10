8234c468314e2c621f05359687b8c8a931444d54
Michael Chong <mikec0920@gmail.com>
2022-06-10 16:04:31 -0700

    Implement user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :username, :password, presence: true
+  validates :username, uniqueness: true
+  validates :username, length: { in: 4..12 }
+  validates :username, format: { with: /\w+/ }
+  validates :password, length: { in: 6..20 }
+  validates :password, format: { with: /\w+/ }
 end
